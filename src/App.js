d9b79f307ccabfadffb0aa49700fbd6f3f3d23b3
Dragos Rusnac <dragos.rusnac@gmail.com>
2021-11-21 16:44:26 +0200

    created log in page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import LogIn from './log-in/LogIn';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <LogIn />
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LogIn />} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
